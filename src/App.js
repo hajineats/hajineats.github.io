cfc2aa4a784ddec60179feb57f20a5d9a600e836
Hajin Kim <68993476+hajineats@users.noreply.github.com>
2021-12-12 23:10:18 +1300

    don't show screen title in mobile

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import "./App.css";
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 import BottomInfo from "./components/bottominfo/BottomInfo";
 import Terminal from "./components/terminal/Terminal";
 import TopDrop from "./components/topdrop/TopDrop";
-import {useState} from "react";
+import { useState } from "react";
 import ProjectList from "./components/projectlist/ProjectList";
 
 const Carousel = styled.div`
   height: 100vh;
   position: relative;
   overflow: hidden;
-`
+`;
 
 const Screens = styled.div`
   height: 100vh;
   width: 300%;
   position: absolute;
   top: 0;
-  ${(props) => props.screen && css`
-    left: ${(props.screen - 1) * -100}%;
-  `};
+  ${(props) =>
+    props.screen &&
+    css`
+      left: ${(props.screen - 1) * -100}%;
+    `};
   transition: 0.5s;
-`
+`;
 
 const Screen = styled.div`
   width: 33.3333%;
@@ -29,17 +31,22 @@ const Screen = styled.div`
   height: 100vh;
   float: left;
 
-  ${(props) => props.first && css`
-    background-color: darkblue;
-  `}
-  ${(props) => props.second && css`
-    background-color: cornflowerblue;
-  `}
-  ${(props) => props.third && css`
-    background-color: midnightblue;
-  `}
-
-`
+  ${(props) =>
+    props.first &&
+    css`
+      background-color: darkblue;
+    `}
+  ${(props) =>
+    props.second &&
+    css`
+      background-color: cornflowerblue;
+    `}
+  ${(props) =>
+    props.third &&
+    css`
+      background-color: midnightblue;
+    `}
+`;
 
 // const Background = styled.div`
 //   background-color: yellowgreen;
@@ -52,29 +59,26 @@ const Centered = styled.div`
   justify-content: center;
 `;
 export default function App() {
-	const [screen, setScreen] = useState(1)
-	return (
-		<>
-
-			<Carousel>
-				<Screens screen={screen}>
-					<Screen first>
-						<Centered>
-							<Terminal />
-						</Centered>
-					</Screen>
-					<Screen second>
-						<Centered>
-							<ProjectList/>
-						</Centered>
-					</Screen>
-					<Screen third/>
-				</Screens>
-			</Carousel>
-			<TopDrop screen={screen} setScreen={setScreen}/>
-			<BottomInfo />
-		</>
-	);
+  const [screen, setScreen] = useState(1);
+  return (
+    <>
+      <Carousel>
+        <Screens screen={screen}>
+          <Screen first>
+            <Centered>
+              <Terminal />
+            </Centered>
+          </Screen>
+          <Screen second>
+            <Centered>
+              <ProjectList />
+            </Centered>
+          </Screen>
+          <Screen third />
+        </Screens>
+      </Carousel>
+      <TopDrop screen={screen} setScreen={setScreen} />
+      <BottomInfo />
+    </>
+  );
 }
-
-
--- src/components/topdrop/TopDrop.js
+++ src/components/topdrop/TopDrop.js
@@ -1,5 +1,5 @@
-import styled, {css} from "styled-components";
-import {useEffect, useState} from "react";
+import styled, { css } from "styled-components";
+import { useEffect, useState } from "react";
 
 const Button = styled.label`
   display: inline-block;
@@ -8,17 +8,17 @@ const Button = styled.label`
   margin: 2px;
   background-color: white;
   cursor: pointer;
-`
+`;
 const TopContainer = styled.div`
   position: absolute;
   top: 20px;
   width: 100%;
   text-align: center;
 
-  ${Button}:nth-child(${props => props.screen}) {
+  ${Button}:nth-child(${(props) => props.screen}) {
     background-color: yellowgreen;
   }
-`
+`;
 
 const ScreenTitle = styled.h1`
   display: block;
@@ -26,60 +26,65 @@ const ScreenTitle = styled.h1`
   background-color: transparent;
   padding: 15px;
   transition: 2s;
-  ${props => props.show && css`
-    color: white;
-    background-color: darkgray;
-  `}
+  ${(props) =>
+    props.show &&
+    css`
+      color: white;
+      background-color: darkgray;
+    `}
 
-  ${props => !props.show && css`
-    color: transparent;
-  `}
-`
+  ${(props) =>
+    !props.show &&
+    css`
+      color: transparent;
+    `}
+`;
 const titles = {
-	1: {
-		title: "Interactive Console"
-	},
-	2: {
-		title: "Projects"
-	},
-	3: {
-		title: "Void"
-	}
+  1: {
+    title: "Interactive Console",
+  },
+  2: {
+    title: "Projects",
+  },
+  3: {
+    title: "Void",
+  },
+};
+export default function TopDrop({ screen, setScreen }) {
+  const [title, setTitle] = useState(titles["1"].title);
+  const [isShowTitle, brieflyShow] = useShowTitle();
+
+  useEffect(() => {
+    brieflyShow();
+  }, [brieflyShow]);
+  const handleSwitchScreen = (toScreen) => {
+    setScreen(toScreen);
+    setTitle(titles[toScreen].title);
+    brieflyShow();
+  };
+
+  return (
+    <TopContainer screen={screen}>
+      <Button onClick={() => handleSwitchScreen(1)} />
+      <Button onClick={() => handleSwitchScreen(2)} />
+      <Button onClick={() => handleSwitchScreen(3)} />
+      {window.innerWidth <= 400 ? (
+        <></>
+      ) : (
+        <ScreenTitle show={isShowTitle}>{title}</ScreenTitle>
+      )}
+    </TopContainer>
+  );
 }
-export default function TopDrop({screen, setScreen}) {
-	const [title, setTitle] = useState(titles["1"].title)
-	const [isShowTitle, brieflyShow] = useShowTitle()
 
+function useShowTitle() {
+  const [isShowTitle, setIsShowTitle] = useState(false);
+  const brieflyShow = () => {
+    setIsShowTitle(true);
+    setTimeout(() => {
+      setIsShowTitle(false);
+    }, 2000);
+  };
 
-	useEffect(() => {
-		brieflyShow()
-	},[brieflyShow])
-	const handleSwitchScreen = (toScreen) => {
-		setScreen(toScreen)
-		setTitle(titles[toScreen].title)
-		brieflyShow()
-	}
-
-	return (
-		<TopContainer screen={screen}>
-			<Button onClick={() => handleSwitchScreen(1)}/>
-			<Button onClick={() => handleSwitchScreen(2)}/>
-			<Button onClick={() => handleSwitchScreen(3)}/>
-			<ScreenTitle show={isShowTitle}>
-				{title}
-			</ScreenTitle>
-		</TopContainer>
-	)
-}
-
-function useShowTitle(){
-	const [isShowTitle, setIsShowTitle] = useState(false)
-	const brieflyShow = () => {
-		setIsShowTitle(true)
-		setTimeout(() => {
-			setIsShowTitle(false)
-		}, 2000)
-	}
-
-	return [isShowTitle, brieflyShow]
+  return [isShowTitle, brieflyShow];
 }
